6351e0bc05897fb84e5a9ed85f5cb1e71d5bc222
Rachana Raut <rachanaraut1999@gmail.com>
2021-02-07 19:18:56 +0530

    Day 6 commit

---
+++ Day 6.java
@@ -0,0 +1,56 @@
+/*
+Binary Tree Right Side View
+Given a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+
+Example:
+
+Input: [1,2,3,null,5,null,4]
+Output: [1, 3, 4]
+Explanation:
+
+   1            <---
+ /   \
+2     3         <---
+ \     \
+  5     4       <---
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List <Integer> result = new ArrayList<>();
+        if(root == null)
+            return result;
+        Queue <TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+         int size = 0;
+        while(!queue.isEmpty()){
+            size = queue.size();
+            
+            for(int i = 1;i <= size; i++){
+                TreeNode temp = queue.poll();
+                if(i == size)
+                    result.add(temp.val);
+                if(temp.left != null)
+                    queue.add(temp.left);
+                if(temp.right != null)
+                    queue.add(temp.right);
+            }
+        
+        }
+        return result;      
+    }
+}
