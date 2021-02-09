3e2ebdbee4ae72ab1ef7a905a716787063ab55de
Rachana Raut <rachanaraut1999@gmail.com>
2021-02-09 21:53:09 +0530

    Day 8 commit

---
+++ Day 8.java
@@ -0,0 +1,48 @@
+/*
+Peeking Iterator
+Given an Iterator class interface with methods: next() and hasNext(), design and implement a PeekingIterator that support the peek() operation -- it essentially peek() at the element that will be returned by the next call to next().
+
+Example:
+
+Assume that the iterator is initialized to the beginning of the list: [1,2,3].
+
+Call next() gets you 1, the first element in the list.
+Now you call peek() and it returns 2, the next element. Calling next() after that still return 2. 
+You call next() the final time and it returns 3, the last element. 
+Calling hasNext() after that should return false.
+Follow up: How would you extend your design to be generic and work with all types, not just integer?
+*/
+// Java Iterator interface reference:
+// https://docs.oracle.com/javase/8/docs/api/java/util/Iterator.html
+
+class PeekingIterator implements Iterator<Integer> {
+	Iterator<Integer> iterator;
+    int cache;
+    boolean cached = false;
+	public PeekingIterator(Iterator<Integer> iterator) {
+	    this.iterator = iterator;
+	    
+	}
+	
+	public Integer peek() {
+        if (!cached) {
+            cache = iterator.next();
+            cached = true;
+        }
+        return cache;
+	}
+	
+	@Override
+	public Integer next() {
+	    if (cached) {
+            cached = false;
+            return cache;
+        }
+        return iterator.next();
+	}
+	
+	@Override
+	public boolean hasNext() {
+	    return (iterator.hasNext() || cached);
+	}
+}
